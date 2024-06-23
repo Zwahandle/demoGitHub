130b71a5fe53492dd301eaa00b2c9f43d6abe9c9
Zwahandle <150572078+Zwahandle@users.noreply.github.com>
2024-06-22 20:04:31 -0700

    bhgcfghfthcthgf

---
+++ Time/Time/TimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeApp.swift
+//  Time
+//
+//  Created by admin on 22/6/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct TimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
